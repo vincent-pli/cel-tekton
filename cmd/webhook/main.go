acdef6ccdf286fd9daf121c0af843bda0d526025
pengli <justdoit.pli@gmail.com>
2021-04-08 08:12:44 +0800

    fix webhook

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -49,7 +49,7 @@ func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher
 	return defaulting.NewAdmissionController(ctx,
 
 		// Name of the resource webhook.
-		fmt.Sprintf("defaulting.webhook.%s.knative.dev", system.Namespace()),
+		fmt.Sprintf("defaulting.webhook.%s.tekton.dev", system.Namespace()),
 
 		// The path on which to serve the webhook.
 		"/defaulting",
@@ -73,7 +73,7 @@ func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher
 	return validation.NewAdmissionController(ctx,
 
 		// Name of the resource webhook.
-		fmt.Sprintf("validation.webhook.%s.knative.dev", system.Namespace()),
+		fmt.Sprintf("validation.webhook.%s.tekton.dev", system.Namespace()),
 
 		// The path on which to serve the webhook.
 		"/resource-validation",
@@ -100,7 +100,7 @@ func NewConfigValidationController(ctx context.Context, cmw configmap.Watcher) *
 	return configmaps.NewAdmissionController(ctx,
 
 		// Name of the configmap webhook.
-		fmt.Sprintf("config.webhook.%s.knative.dev", system.Namespace()),
+		fmt.Sprintf("config.webhook.%s.tekton.dev", system.Namespace()),
 
 		// The path on which to serve the webhook.
 		"/config-validation",
