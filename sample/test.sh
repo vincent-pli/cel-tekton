d0d89f95cbef83a1242c64d18aa5a4ff39c1760f
root <justdoit.pli@gmail.com>
2021-04-08 18:43:55 -0700

    refine the example

--- sample/test.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-         doit() {
-           kubectl get node
-           echo Done with $1
-         }
-         export -f doit
-         parallel doit ::: 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9
